5a63be9b568d9effcb3fd61ae366fb1bbcd6c14f
Sunless <scralen01@hotmail.com>
2024-09-02 21:12:00 +0300

    added another of my own projects that needs work

---
+++ My-Projects/PortofolioProjects/ToDoList/app.js
@@ -0,0 +1,83 @@
+// Model Section
+let todos = [
+  {
+    title: "Get groceries",
+    dueDate: "2021-10-04",
+    id: "id1",
+  },
+  {
+    title: "Wash car",
+    dueDate: "2021-02-03",
+    id: "id2",
+  },
+  {
+    title: "Make dinner",
+    dueDate: "2021-03-04",
+    id: "id3",
+  },
+];
+
+// Creates a todo
+function createTodo(title, dueDate) {
+  const id = "" + new Date().getTime();
+
+  todos.push({
+    title: title,
+    dueDate: dueDate,
+    id: id,
+  });
+}
+
+// Deletes a todo
+function removeTodo(idToDelete) {
+  todos = todos.filter(function (todo) {
+    if (todo.id === idToDelete) {
+      return false;
+    } else {
+      return true;
+    }
+  });
+}
+
+render();
+
+// Controller Section
+function addTodo() {
+  const textbox = document.getElementById("todo-title");
+  const title = textbox.value;
+
+  const datePicker = document.getElementById("date-picker");
+  const dueDate = datePicker.value;
+
+  createTodo(title, dueDate);
+  render();
+}
+
+function deleteTodo(event) {
+  const deleteButton = event.target;
+  const idToDelete = deleteButton.id;
+  removeTodo(idToDelete);
+  render();
+}
+
+// View Section
+function render() {
+  // rest our list
+  document.getElementById("todo-list").innerHTML = "";
+
+  todos.forEach(function (todo) {
+    const element = document.createElement("div");
+    element.innerText = todo.title + " " + todo.dueDate;
+
+    const deleteButton = document.createElement("button");
+    deleteButton.innerText = "Remove";
+    deleteButton.style = "margin-left: 12px";
+    deleteButton.onclick = deleteTodo;
+    deleteButton.id = todo.id;
+
+    element.appendChild(deleteButton);
+
+    const todoList = document.getElementById("todo-list");
+    todoList.appendChild(element);
+  });
+}
